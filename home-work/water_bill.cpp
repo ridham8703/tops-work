3012e9e21a225163db81d21ee041c6bf4bc5b49e
ridham <ridham8703j@gmail.com>
2025-01-25 13:57:59 +0530

    new update

--- home-work/water_bill.cpp
+++ home-work/water_bill.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     cout<<"Welcome!!! to surat municiple corporation..."<<endl<<"---------------------------------------------"<<endl;
-    int current_unit , previous_unit , total_consumed_unit;
+    int current_unit , previous_unit , total_consumed_unit , bill;
         cout<<"Please enter your current unit: "<<endl;
         cin>>current_unit;
         
@@ -19,16 +19,36 @@ int main() {
         cout<< "Please check your meter "<< endl;
     }
                 cout<<"--------------------------------"<< endl;
-                if(total_consumed_unit >= 0){
-    if(total_consumed_unit == 0 && total_consumed_unit <= 100){
-        cout<<"Your bill is: "<<total_consumed_unit*10<<"₹";
-    }else if(total_consumed_unit == 101 && total_consumed_unit <= 250){
-        cout<<"Your bill is: "<<total_consumed_unit*15<<"₹";
-    }else if(total_consumed_unit == 251 && total_consumed_unit <= 350){
-        cout<<"Your bill is: "<<total_consumed_unit*25<<"₹";
-    }else{
-        cout<<"Your bill is: "<<total_consumed_unit*35<<"₹";
-    }
+                if (total_consumed_unit >= 0) {
+        if (total_consumed_unit <= 100) {
+            bill = total_consumed_unit * 10;
+        } else if (total_consumed_unit <= 250) {
+            bill = total_consumed_unit * 15;
+        } else if (total_consumed_unit <= 350) {
+            bill = total_consumed_unit * 25;
+        } else {
+            bill = total_consumed_unit * 35;
+        }
                 }
+                cout<<"total bill : "<<bill <<endl;
+                cout<<"--------------------------------"<< endl;
+                if (bill > 5000) {
+        bill -= (bill * 0.30); 
+    } else if (bill > 2000) {
+        bill -= (bill * 0.20); 
+    } else if (bill > 1000) {
+        bill -= (bill * 0.10); 
+    }
+
+    cout << "Your total bill after discount is: " << bill << endl;
+                
+                
+                
     return 0;
 }
+
+
+
+//1000 uper bill hoi to 10% dicount
+//2000 uper bill hoi to 20% dicount
+//5000 uper bill hoi to 30% dicount
